2b37fa1af7f912cce380f27d11d0a9292667e022
chichovski <cheketa.viktor@gmail.com>
2017-02-14 03:50:08 +0200

    Index migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213235429) do
+ActiveRecord::Schema.define(version: 20170214014748) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
